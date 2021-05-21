69ebf421c875d81c706097720db29bfbaddaf7c8
Anton Smirnov <antosha-85@yandex.ru>
2021-05-20 22:15:12 -0600

    Updated ProductDetailScreen that is using now loadedProudct using Provider and from there we extract the specific product along with the title

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -7,9 +9,12 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<ProductsProvider>(context)
+        .items
+        .firstWhere((product) => product.id == productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
